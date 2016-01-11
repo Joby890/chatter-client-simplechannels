8eee3dceffc87ebcc0b8c379225691639e0acb6f
Joseph Brumaghim <joe@josephbrumaghim.com>
2016-01-10 21:10:06 -0500

    Fix issue when relogging in and Add button not showing up

--- sc.js
+++ sc.js
@@ -3,48 +3,63 @@
 
 export default function(chatter) {
   console.log("Simple Channel started");
+
   this.onEnable = function() {
+    var plugin = this;
+    if(this.hasAuthed) {
+      setup(plugin);
+    }
+    //this.hasAuthed = false;
+
     chatter.pluginManager.registerEvent(this, "AfterAuthEvent", function(event) {
-      var page;
-      var AddChannel = React.createClass({
-
-        componentDidMount:function() {
-          var self = this;
-          console.log(self, " mounted")
-        },
-
-        addChannel: function() {
-          chatter.send("createChannel", {name: this.channelName.value});
-          this.channelName.value = "";
-          chatter.getPanel('right').removePage(page);
-          console.log(chatter.getPanel('right').hasPage(page))
-        },
-        render: function() {
-          return React.createElement("div", null,
-          React.createElement("input", {ref: function(c)  {return this.channelName = c;}.bind(this)}),
-          React.createElement("input", {type:"button", onClick: this.addChannel, value: "Add Channel"})
-        );
-      }
+      plugin.hasAuthed = true;
+      setup(plugin);
     });
-    page = {weight: .1, component: AddChannel};
-    var CreateButton = React.createClass({
-      showChannel: function() {
-        if(!chatter.getPanel('right').hasPage(page)) {
-          chatter.getPanel('right').addPage(page);
-        }
+
+};
+
+
+function setup(plugin) {
+    var page;
+    var AddChannel = React.createClass({
+
+      componentDidMount:function() {
+        var self = this;
+        console.log(self, " mounted");
       },
 
+      addChannel: function() {
+        chatter.send("createChannel", {name: this.channelName.value});
+        this.channelName.value = "";
+        chatter.getPanel('right').removePage(page);
+        console.log(chatter.getPanel('right').hasPage(page));
+      },
       render: function() {
-        return React.createElement("input", {type:"button", onClick: this.showChannel, value: "Add"});
+        return React.createElement("div", null,
+        React.createElement("input", {ref: function(c)  {
+          this.channelName = c;
+          return this.channelName;
+        }.bind(this)}),
+        React.createElement("input", {type:"button", onClick: this.addChannel, value: "Add Channel"})
+      );
+    }
+  });
+  page = new Page(0.1,AddChannel);
+  var CreateButton = React.createClass({
+    showChannel: function() {
+      if(!chatter.getPanel('right').hasPage(page)) {
+        chatter.getPanel('right').addPage(plugin, page);
       }
-    })
+    },
 
-    console.log("Adding Channel to right below channels");
-    chatter.getPanel('left').addPage({weight: 2, component: CreateButton});
+    render: function() {
+      return React.createElement("input", {type:"button", onClick: this.showChannel, value: "Add"});
+    }
+  });
 
-  })
-
-  }
+  console.log("Adding Channel to right below channels");
+  chatter.getPanel('left').addPage(plugin, new Page(2, CreateButton));
+}
 
 
 }
